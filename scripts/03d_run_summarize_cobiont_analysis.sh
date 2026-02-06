357ef33e65ef475c00fbd4a3aa96a2596a5eae5b
larabatman <lara.land8@outlook.com>
2026-02-06 16:37:57 +0100

    QC analysis summary

---
+++ scripts/03d_run_summarize_cobiont_analysis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#SBATCH --partition=pibu_el8
+#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --job-name=summarize_cobiont_analysis
+#SBATCH --output=logs/summarize_cobiont_analysis_%j.out
+#SBATCH --error=logs/summarize_cobiont_analysis_%j.err
+
+# SLURM wrapper running the R script which summarizes the cobiont QC analysis otuputs
+
+set -euo pipefail
+
+# Load R
+module load R/4.2.1-foss-2021a
+
+# Project root
+WORKDIR="/data/projects/p2025-0083_a_cross-species_pipeline_for_mining_cobionts_in_hymenopteran_genomes"
+cd "$WORKDIR"
+
+# Run QC script
+Rscript scripts/03d_summarize_cobiont_analysis.R
