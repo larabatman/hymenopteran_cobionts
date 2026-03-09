f1bedfed4f68d4d1e7f43930491630a5507ec288
larabatman <lara.land8@outlook.com>
2026-03-09 13:38:36 +0100

    Merqury: QV and completeness

---
+++ scripts/stages/A3_assembly_consensus.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=assembly_merqury
+#SBATCH --partition=pibu_el8
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=120G
+#SBATCH --output=logs/assembly_merqury_%j.out
+#SBATCH --error=logs/assembly_merqury_%j.err
+
+# Stage A: Assembly
+# A3: Merqury consensus
+# Input: hifiasm primary assembly
+# This script aims to further produce useful assembly metrics, such as QV and completeness
+
+# 1) Setup 
+set -euo pipefail
+
+SPECIES="$1"
+ASM_MODE="$2"
+
+WORKDIR="/data/projects/p2025-0083_mining_cobionts"
+cd "$WORKDIR"
+
+ASM="${WORKDIR}/assemblies/hifiasm_clean/${SPECIES}/asm.${ASM_MODE}.p_ctg.fasta"
+
+OUTDIR="${WORKDIR}/results/${SPECIES}_stages/assembly_qc/merqury"
+mkdir -p "$OUTDIR"
+
+CONTAINER="/containers/apptainer/merqury_1.3.sif"
+export MERQURY="/usr/local/share/merqury"
+
+RAW_DB="$OUTDIR/hifi_raw.meryl"
+MERYL_DB="$OUTDIR/hifi.meryl"
+HIFI_READ="reads/pacbio_hifi/${SPECIES}"/*.fastq.gz
+
+echo "[INFO] Species: $SPECIES"
+echo "[INFO] Assembly: $ASM"
+echo "[INFO] Output dir: $OUTDIR"
+
+# 2) Build raw k-mer database
+if [[ ! -d "$RAW_DB" ]]; then
+    echo "[INFO] Building raw meryl database"
+
+    apptainer exec --bind /data "$CONTAINER" \
+        meryl count k=21 \
+        memory=90 \
+        threads=$SLURM_CPUS_PER_TASK \
+        output "$RAW_DB" \
+        "$HIFI_READ"
+fi
+
+# 3) Remove singleton kmers from sequencing errors
+if [[ ! -d "$MERYL_DB" ]]; then
+    echo "[INFO] Filtering singleton kmers"
+
+    apptainer exec --bind /data "$CONTAINER" \
+        meryl greater-than 1 "$RAW_DB" \
+        output "$MERYL_DB"
+fi
+
+# 4) Run Merqury
+RUN_DIR="$OUTDIR/${SPECIES}"
+mkdir -p "$RUN_DIR"
+
+echo "[INFO] Running Merqury"
+
+apptainer exec --bind /data "$CONTAINER" bash -lc '
+export MERQURY=/usr/local/share/merqury
+cd "'"$RUN_DIR"'"
+"$MERQURY/merqury.sh" "'"$MERYL_DB"'" "'"$ASM"'" "'"$SPECIES"'"
+'
+
+# Done
+echo "[INFO] Merqury finished for $SPECIES"
