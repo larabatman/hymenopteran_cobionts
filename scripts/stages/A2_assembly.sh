b62bf11b79a5d91d80ef7c8498a26d481584e310
larabatman <lara.land8@outlook.com>
2026-03-11 11:00:27 +0100

    hifiasm assembly

--- scripts/stages/A2_assembly.sh
+++ scripts/stages/A2_assembly.sh
@@ -1,97 +1,94 @@
-#!/usr/bin/bash
-#SBATCH --job-name=reads_qc
+#!/usr/bin/env bash
+#SBATCH --job-name=hifiasm_assembly
 #SBATCH --partition=pibu_el8
-#SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=32G
-#SBATCH --output=logs/reads_qc_%j.out
-#SBATCH --error=logs/reads_qc_%j.err
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=120G
+#SBATCH --output=logs/hifiasm_assembly_%j.out
+#SBATCH --error=logs/hifiasm_assembly_%j.err
 
-# Stage A: Assembly 
-# A1: General read QC
-# Overall HiFi and Hi-C statistics
-# Output: fastqc reports
-
-set -euo pipefail
+# Stage A: Assembly
+# A2: hifiasm assembly
+# Assemble raw HiFi reads, exploiting Hi-C contact information for phasing when available 
+# Supports:
+#   - HiFi-only assembly (ASM_MODE=bp)
+#   - HiFi + Hi-C assembly (ASM_MODE=hic)
 
 # 1) Setup
+set -euo pipefail
+
+# Variables
 SPECIES="$1"
 ASM_MODE="$2"
-
-echo "[INFO] job started on $(hostname)"
-echo "[INFO] species=$SPECIES"
-echo "[INFO] assembly mode=$ASM_MODE"
 
 # Paths
 WORKDIR="/data/projects/p2025-0083_mining_cobionts"
 cd "$WORKDIR"
 
-QC_DIR="results/${SPECIES}_stages/read_qc"
-HIC_CLEAN="reads/hic_clean/${SPECIES}"
+OUTDIR="assemblies/hifiasm_clean/${SPECIES}"
+PREFIX="${OUTDIR}/asm"
 
-mkdir -p "$QC_DIR" "$HIC_CLEAN" logs
+mkdir -p "$OUTDIR" logs
 
 HIFI=(reads/pacbio_hifi/${SPECIES}/*.fastq.gz)
 
-HIC_R1="reads/hic/${SPECIES}/hic_R1.fastq.gz"
-HIC_R2="reads/hic/${SPECIES}/hic_R2.fastq.gz"
+THREADS=${SLURM_CPUS_PER_TASK}
 
-THREADS=${SLURM_CPUS_PER_TASK:-1}
+# Modules
+module load hifiasm/0.16.1-GCCcore-10.3.0
 
-echo "[INFO] Using $THREADS threads"
+# Logs and checks
+echo "[INFO] Species: $SPECIES"
+echo "[INFO] Assembly mode: $ASM_MODE"
 
-# Load modules
-module load FastQC/0.11.9-Java-11
-#module load fastp/0.23.4-GCC-10.3.0
-module load SeqKit/2.6.1
+for f in "${HIFI[@]}"; do
+    [[ -s "$f" ]] || { echo "ERROR: missing HiFi file $f"; exit 1; }
+done
 
-# 2) HiFi QC
-echo "[INFO] Running HiFi QC"
-
- seqkit stats "${HIFI}" > "$QC_DIR/hifi_stats.tsv"
-
-fastqc \
-  -t "$THREADS" \
-  -o "$QC_DIR" \
-  "${HIFI}"
-
-
-# End of script for ASM_MODE=="bp"
+# 2a) Run hifiasm, HiFi only mode
 if [[ "$ASM_MODE" == "bp" ]]; then
-    echo "[INFO] ASM_MODE=bp: skipping Hi-C QC"
-    echo "[INFO] HiFi QC completed successfully"
-    exit 0
+
+    echo "[INFO] Running HiFi-only assembly"
+
+    hifiasm \
+      -o "$PREFIX.bp" \
+      -t "$THREADS" \
+      "${HIFI[@]}"
+
+    GFA="${PREFIX}.bp.p_ctg.gfa"
+    FASTA="${PREFIX}.bp.p_ctg.fasta"
+
 fi
 
-# 3) Hi-C QC
-echo "[INFO] Running Hi-C raw QC"
+# 2b) Run hifiasm, HiFi + Hi-C mode
+if [[ "$ASM_MODE" == "hic" ]]; then
 
-fastqc \
-  -t "$THREADS" \
-  -o "$QC_DIR" \
-  "$HIC_R1" "$HIC_R2"
-  
-# Optional trim, but dangerous
-# fastp can desynchronize pairs → disabled for now
+    R1="reads/hic/${SPECIES}/hic_R1.fastq.gz"
+    R2="reads/hic/${SPECIES}/hic_R2.fastq.gz"
 
-# fastp \
-#   -i "$HIC_R1" \
-#   -I "$HIC_R2" \
-#   -o "$HIC_CLEAN/hic_R1.clean.fastq.gz" \
-#   -O "$HIC_CLEAN/hic_R2.clean.fastq.gz" \
-#   --detect_adapter_for_pe \
-#   --trim_poly_g \
-#   --thread "$THREADS" \
-#   --html "$QC_DIR/fastp_report.html" \
-#   --json "$QC_DIR/fastp_report.json"
+    [[ -s "$R1" ]] || { echo "ERROR: missing Hi-C R1"; exit 1; }
+    [[ -s "$R2" ]] || { echo "ERROR: missing Hi-C R2"; exit 1; }
 
-#echo "[INFO] Running Hi-C QC after trimming"
+    echo "[INFO] Running HiFi + Hi-C assembly"
 
-#fastqc \
-#  -t "$THREADS" \
-#  -o "$QC_DIR" \
-#  "$HIC_CLEAN/hic_R1.clean.fastq.gz" \
-#  "$HIC_CLEAN/hic_R2.clean.fastq.gz"
+    hifiasm \
+      -o "$PREFIX.hic" \
+      -t "$THREADS" \
+      --h1 "$R1" \
+      --h2 "$R2" \
+      "${HIFI[@]}"
 
-# Done 
-echo "[INFO] A1 Read QC completed successfully"
+    GFA="${PREFIX}.hic.p_ctg.gfa"
+    FASTA="${PREFIX}.hic.p_ctg.fasta"
+
+fi
+
+# Convert GFA to FASTA
+echo "[INFO] Converting GFA to FASTA"
+
+[[ -s "$GFA" ]] || { echo "ERROR: GFA missing: $GFA"; exit 1; }
+# awk command to select sequences S lines from GFA
+awk '/^S/{print ">"$2"\n"$3}' "$GFA" > "$FASTA"
+
+echo "[INFO] Assembly finished"
+echo "[INFO] FASTA: $FASTA"
