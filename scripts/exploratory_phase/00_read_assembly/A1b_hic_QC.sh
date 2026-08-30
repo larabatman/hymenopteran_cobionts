6e49b910d94ba0d4e79e8a01ab7865e3abf497e1
larabatman <lara.land8@outlook.com>
2026-08-30 08:04:32 +0200

    Dropped correction parameter

--- scripts/exploratory_phase/00_read_assembly/A1b_hic_QC.sh
+++ scripts/exploratory_phase/00_read_assembly/A1b_hic_QC.sh
@@ -7,25 +7,26 @@
 #SBATCH --output=/data/projects/p2025-0083_mining_cobionts/logs/hic_qc_%j.out
 #SBATCH --error=/data/projects/p2025-0083_mining_cobionts/logs/hic_qc_%j.err
 
-# fastp cleaning adapter trim, Q20, min 50bp + FastQC + seqkit stats
-#
-# Usage: sbatch A1b_hic_qc.sh <species>
+# Clean HiC raw reads: fastp cleaning for adapter trimming, Q20 and minimum 50bp
+# FastQC reports and seqkit stats
+# Usage: 
+# sbatch A1b_hic_qc.sh species
 
 set -euo pipefail
 
 SPECIES="$1"
 
-# Paths
+# Working directory
 WORKDIR="/data/projects/p2025-0083_mining_cobionts"
 cd "$WORKDIR"
-
+# Clean directory
 HIC_QC_DIR="results/${SPECIES}_stages/read_qc/hic_qc"
 HIC_CLEAN_DIR="reads/hic_clean/${SPECIES}"
 mkdir -p "$HIC_QC_DIR" "$HIC_CLEAN_DIR" logs
-
+# Raw reads directory
 HIC_R1="reads/hic/${SPECIES}/hic_R1.fastq.gz"
 HIC_R2="reads/hic/${SPECIES}/hic_R2.fastq.gz"
-
+# Clean reads directory
 HIC_CLEAN_R1="${HIC_CLEAN_DIR}/hic_R1.clean.fastq.gz"
 HIC_CLEAN_R2="${HIC_CLEAN_DIR}/hic_R2.clean.fastq.gz"
 
@@ -37,11 +38,14 @@ module load FastQC/0.11.9-Java-11
 module load fastp/0.23.4-GCC-10.3.0
 module load SeqKit/2.6.1
 
-# RAW stats + FastQC
+# Extract raw statistics
+# Launch FastQC report 
 seqkit stats -T "$HIC_R1" "$HIC_R2" > "${HIC_QC_DIR}/hic_raw_stats.tsv"
 fastqc -t "$THREADS" -o "$HIC_QC_DIR" "$HIC_R1" "$HIC_R2"
 
-# fastp cleaning
+# Clean reads with fastp: 
+# --detect_adapter_for_pe autodetects adapters in pair end reads
+# set minimal length at 50 bp, quality score at Q20, and minimal qualification over 40% of the read
 fastp \
     --in1 "$HIC_R1" \
     --in2 "$HIC_R2" \
@@ -51,11 +55,10 @@ fastp \
     --qualified_quality_phred 20 \
     --unqualified_percent_limit 40 \
     --length_required 50 \
-    --correction \
     --thread "$THREADS" \
     --html "${HIC_QC_DIR}/fastp_hic_report.html" \
     --json "${HIC_QC_DIR}/fastp_hic_report.json"
 
-# Clean stats + FastQC
+# Extract clean statistics and FastQC report
 seqkit stats -T "$HIC_CLEAN_R1" "$HIC_CLEAN_R2" > "${HIC_QC_DIR}/hic_clean_stats.tsv"
 fastqc -t "$THREADS" -o "$HIC_QC_DIR" "$HIC_CLEAN_R1" "$HIC_CLEAN_R2"
